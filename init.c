aaddcb24b2ea17445ab4aea9d07273f404600cdd
LProtox <36269176+LProtox@users.noreply.github.com>
2019-02-11 21:11:48 -0600

    Add files via upload

--- init.c
+++ init.c
@@ -3,48 +3,51 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-#define MAX_FORK 5
+#include <sys/types.h>
 
 #define FALSE 0
 #define TRUE 1
+#define ERROR -1
+
+#define XTERM_PATH "/usr/bin/xterm"
+#define XTERM "xterm"
 
 int main() 
 {
+	/**/
+	int exec_status = 0;
     /*Variable para el control de los procesos hijos*/
-    int p_hijos = 6;
+    int p_hijos = 0;
     int pid = 0;
-    int status = 0;
+    int status;
     /*Almacenamos los pids de los procesos hijos para tener un control*/
-    char pid_buffer[MAX_FORK] = {0};
+    char pid_buffer[20] = {0};
     
-    printf("check1\n");
-    
-    while(1)
+    while(TRUE)
     {
         /*Variable donde se almacenara el pid de cada proceso hijo*/
         pid = fork();
         /*Restamos 1 a la variable para el control de los procesos hijos*/
-        p_hijos--;
-        
-        printf("check2\n");
+        p_hijos++;
         
         /*Obtenemos el pid del proceso padre para almacenarlo en el buffer*/
         /*snprinf permite almacenar el valor del 3er argumento en el 1er arg limitantolo a un valor de 5 para seguridad*/
-        snprintf(pid_buffer,MAX_FORK,"%d",getpid());
-        
-        printf("check3\n");
+        /*getppid() obtiene el pid del proceso padre del proceso actual*/
+        sprintf(pid_buffer,"%d",getppid());
         
         if(FALSE == pid)
         {
             /*Mandar a llamar funcion execl*/
             /*Se generan las ventanas y se manda a llamar el programa .getty*/
-            //execl("/usr/bin/xterm","/usr/bin/xterm", "-e","./getty", pid_buffer, (void*)NULL);
-            printf("Ventana\n");
+            exec_status = execl(XTERM_PATH,XTERM,"-hold","-e","./getty","getty",(char*)NULL);
+            if(ERROR == exec_status)
+            {
+				perror("Failed to execute");
+			}
         }
         else
         {
-            if(p_hijos < TRUE)
+            if(p_hijos >= 6)
             {
                 /*Revisa el estado del sistema*/
                 wait(&status);
@@ -52,7 +55,7 @@ int main()
                 /*Hace que el sistema entre en modo hibernacion hasta que le entre una llamada*/
                 sleep(1);
                 /*Si sale de modo hibernacion se le aumenta a p_hijos para abrir otra ventana*/
-                p_hijos++;
+                p_hijos--;
             }
         }
     }
