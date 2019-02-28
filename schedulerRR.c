f65fd0170f5e72b8dcce344e7be458924c288dbe
Luis <36269176+LProtox@users.noreply.github.com>
2019-02-28 13:17:47 -0600

    RR

---
+++ schedulerRR.c
@@ -0,0 +1,66 @@
+#include <scheduler.h>
+
+extern THANDLER threads[MAXTHREAD];
+extern int currthread;
+extern int blockevent;
+extern int unblockevent;
+
+QUEUE ready;
+QUEUE waitinginevent[MAXTHREAD];
+
+void scheduler(int arguments)
+{
+	int old,next;
+	int changethread=0;
+	int waitingthread=0;
+	
+	int event=arguments & 0xFF00;
+	int callingthread=arguments & 0xFF;
+
+	if(event==NEWTHREAD)
+	{
+		// Un nuevo hilo va a la cola de listos
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+	}
+	
+	if(event==BLOCKTHREAD)
+	{
+
+		threads[callingthread].status=BLOCKED;
+		_enqueue(&waitinginevent[blockevent],callingthread);
+
+		changethread=1;
+	}
+
+	if(event==ENDTHREAD)
+	{
+		threads[callingthread].status=END;
+		changethread=1;
+	}
+	
+	if(event==UNBLOCKTHREAD)
+	{
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+	}
+	/*Respecto al documento lo unico que falta es el evento TIMER
+	 * Activacion del reloj*/
+	if(event==TIMER)
+	{
+		threads[callingthread].status = READY;
+		_enqueue(&ready,callingthread);
+		
+		changethread = TRUE;
+	}
+	
+	if(changethread)
+	{
+		old=currthread;
+		next=_dequeue(&ready);
+		
+		threads[next].status=RUNNING;
+		_swapthreads(old,next);
+	}
+
+}
